f6e2be847c47d436d9f8d5c7bb5f10fa5faef627
yeomyeonGit <yeomyeongforbusiness@gmail.com>
2025-01-08 16:02:34 +0900

    while_dumb

--- Python_Grammar basics/Python for Eggs/03_while.py
+++ Python_Grammar basics/Python for Eggs/03_while.py
@@ -1,3 +1,5 @@
+'''
+
 start = 1
 number = int(input('정수를 입력하세요: '))
 a = 0
@@ -5,3 +7,33 @@ a = 0
 while start <= number:
     print(number)
     start += 1
+    
+    '''
+
+''' number = int(input('정수를 입력하세요: '))
+i = 1
+
+while i <= number:
+    print(i, i * i)
+    i += 1
+    '''
+
+# i = 1
+# popping = 100 * 0.6
+
+# while i <= 10:
+#     print(i, round(popping, 4))
+#     i += 1
+#     popping *= 0.6
+
+#or
+
+height = 100
+bounce = 3 / 5
+
+i = 1
+
+while i <= 10:
+    height = height * bounce
+    print(i, round(height, 4))
+    i += 1
