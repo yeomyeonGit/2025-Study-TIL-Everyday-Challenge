dafe30d0893830c8cfb08e291263e1f2271927fa
yeomyeonGit <yeomyeongforbusiness@gmail.com>
2025-01-09 11:32:32 +0900

    250109

---
+++ 06_function.py
@@ -0,0 +1,52 @@
+#매개 변수가 있는 함수
+'''
+
+a_list = [3, 4, 62, 27, 83, 956, 26, 58, 3 ,78, 168, 64, 78]
+
+def print_list(a):
+    for i in a:
+        print(i)
+
+print_list(a_list)
+
+'''
+
+#매개 변수가 없는 함수
+
+def boy():
+    print('I am a boy')
+    print('You are a gitl')
+
+
+#혼자 연습
+
+def compare(a, b):
+    if a > b:
+        print('a > b')
+    elif a < b:
+        print('a < b')
+    else:
+        print('a == b')
+
+#연습 문제 1
+
+def length(a):
+    print(len((a)))
+
+
+#연습 문제 2
+
+def gugudan():
+    a = 2
+    while a <= 9:
+        for i in range (1, 10):
+            print(f'{a} * {i} = {a * i}')
+    
+        a += 1
+    
+
+
+        
+
+
+gugudan()
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 *~for, while까지
 *복습할 것: 윤년 판별 로직, for-else, while-else break 시 모습
 *마크다운 쓰는 법. 진짜... 이게 뭐냐?
+*function, 반복 중첩문 - 연습문제 2
